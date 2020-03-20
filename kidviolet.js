3bc8ac1dd5f0cb010786af0a2ba02b88a9085a95
Kid-Violet <50734982+Kid-Violet@users.noreply.github.com>
2020-03-20 14:36:31 +0000

    Add Twitter

--- kidviolet.js
+++ kidviolet.js
@@ -21,16 +21,16 @@ function InitLinkEvents()
         window.open('https://open.spotify.com/artist/18Kupu2OZy7drYCfFlIxfz?si=eAQy4VfJRp2TlNtn0ZXuzw', '_blank');
     });
 
-	$("#Teemill").on('click', function() {
-        window.open('https://kidviolet.teemill.com/', '_blank');
-    });
-
     $("#Facebook").on('click', function() {
         window.open('https://www.facebook.com/Kid.Violet/', '_blank');
     });
 
     $("#Instagram").on('click', function() {
         window.open('https://www.instagram.com/kid.violet/', '_blank');
+    });
+	
+    $("#Twitter").on('click', function() {
+        window.open('https://twitter.com/KidVioletBand', '_blank');
     });
 }
 
